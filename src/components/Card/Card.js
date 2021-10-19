51fa8a61d0ecce4635c379998545ad9efa7e71a2
hoangcam2000 <hoangcam2000@gmail.com>
2021-10-19 15:42:26 +0700

    implement Drag and Drop, eslint

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,18 +3,22 @@ import React from 'react'
 import './Card.scss'
 
 
-
 function Card(props) {
-    const { card }= props
+  const { card }= props
 
 
-    return  <li className = "card-item">
-        
-        {card.cover && <img src={card.cover} alt="avatar" className="card-cover"></img> }
-        {card.title}
-        
-               
-            </li>
+  return (
+    <div className = "card-item">
+      {card.cover &&
+         <img
+           src={card.cover}
+           alt="avatar"
+           className="card-cover"
+           onMouseDown={e => e.preventDefault()}
+         />
+      }
+      {card.title}
+    </div>
+  )
 }
- 
 export default Card
